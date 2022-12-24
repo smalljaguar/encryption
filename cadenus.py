47ab0b62aa05e5b8e6becd784c2ed4813a3352d1
Isaac <i.hall@cityoflondonschool.org.uk>
2022-12-24 01:51:00 +0000

    giving up on hillclimb, seems broken? Now trying to do cribbed decrypt (2am commit btw)

--- cadenus.py
+++ cadenus.py
@@ -8,10 +8,11 @@ from collections import Counter
 def rotate(text, offset):
     return text[-offset:]+text[:-offset]
 
-# factors are [2, 5, 10, 13, 26, 65] for our text, possible keylens probably 5, maybe 10,
-# as 25**n possibilities, so 95_367_431_640_625==2**46 if n=10; could be done overnight?,
-# but if n=13, ~2**60 possibilities, would require 10**13 checks per second naively
-# This code runs at ~20_000 calls a second, so 1_728_000_000 checks a day, or just under 2**31
+# factors are [2, 5, 10, 13, 25, 26, 50, 65, 125, 130, 250, 325, 650, 1625] for our text
+# keylen probably 13
+# blocksizes must be in [2, 5, 10, 13, 25, 26, 50, 65, 125, 130, 250, 325] probably > 10 maybe > 25, def != 25
+# blocksize * keylen must be factor of 3250
+# so say keylen 13 and blocksize 50, 13*50 = 650, in list so valid
 
 
 def cadenus_encrypt(plaintext: str, key: str):
@@ -23,9 +24,10 @@ def cadenus_encrypt(plaintext: str, key: str):
     ciphertext = ""
     offsets = []
     for char in list(Counter(key).keys()):
-        offsets.append(21) if char == "w" else offsets.append(
+        offsets.append(21) if char == "w" else offsets.append(  # skipped letter is entirely arbitrary
             "abcdefghijklmnopqrstuvxyz".index(char))
 
+    # doesn't have to be 25, has to be in [2, 5, 10, 13, 25, 26, 50, 65, 125, 130, 250, 325, 650, 1625]
     blocksize = len(offsets)*25
 
     blocks = [plaintext[i:i+blocksize]
@@ -84,26 +86,27 @@ pair_ref = pair_normalised_common_ref(4)
 
 
 def fitness(text):
-    textcount = {key: count
-                 for key, count in Counter(nwise(text, n=4)).items()}
+    """
+    Real english around or below -2?
+    """
+    textcount = {key: count/(len(text)*4)
+                 for key, count in Counter(nwise(text.lower(), n=4)).items()}
     error = 0
     for key in pair_ref:
         if key in textcount:
-            error += abs(log(pair_ref[key])*textcount[key])
-        else:
-            error += pair_ref[key]
-    return error
+            error += log(pair_ref[key])*textcount[key]
+    return -error
 
 
-def decipher(ciphertext, offsets, perm):
+def decipher(ciphertext, offsets, perm, keylen, block_mult=25):
     # TODO
     plaintext = ""
     unsorter = perm
-    blocksize = len(offsets)*25
+    blocksize = len(offsets)*block_mult
     blocks = [ciphertext[i:i+blocksize]
               for i in range(0, len(ciphertext), blocksize)]
     for block in blocks:
-        rows = [block[i:i+5] for i in range(0, len(block), 5)]
+        rows = [block[i:i+keylen] for i in range(0, len(block), keylen)]
 
         columns = ["".join(x) for x in zip(*rows)]
         sorted_columns = [x[1]
@@ -116,26 +119,26 @@ def decipher(ciphertext, offsets, perm):
     return plaintext
 
 
-def hill_climb(ciphertext):
+def hill_climb(ciphertext, keylen=5, blocksize=25):
     # attack rolling and columnar transposition separately
     # blatantly stolen from https://www.cipherchallenge.org/wp-content/uploads/2022/09/A-Book-on-Classical-Cryptography-by-Madness.pdf # noqa: E501
-    keylen = 5
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
-    parent_shifts = [0, 0, 0, 0, 0]
-    parent_perms = [0, 1, 2, 3, 4]
+    parent_shifts = [0 for _ in range(keylen)]
+    parent_perms = [x for x in range(keylen)]
     best_fitness = fitness(ciphertext)
     count = 0
-    cutoff = -.5
-    while count < 1_000:
+    cutoff = 1.8
+    while count < 100_000:
         child_shifts = parent_shifts
         child_perms = parent_perms
         choice = random.choice([0, 1, 2, 3])
         if choice == 0:
-            # change one member of the child shift key to a random number in 0, ..., 25
-            child_shifts[random.randint(0, 4)] = random.randint(0, 24)
+            # change one member of the child shift key to a random number in 0, ..., blocksize
+            child_shifts[random.randint(
+                0, keylen-1)] = random.randint(0, blocksize-1)
         elif choice == 1:
             # swap two randomly selected members of the child permutation
-            a, b = random.sample([0, 1, 2, 3, 4], 2)
+            a, b = random.sample(parent_perms, 2)
             child_perms[a], child_perms[b] = child_perms[b], child_perms[a]
         elif choice == 2:
             # randomly choose a number n from 1 to the key length
@@ -144,13 +147,16 @@ def hill_climb(ciphertext):
             n = random.randint(1, keylen)
             child_shifts = rotate(child_shifts, n)
             child_perms = rotate(child_perms, n)
+            child_shifts[-n:] = [(item-1) % 25 for item in child_shifts[-n:]]
+            ...
         elif choice == 3:
-            # randomly choose a number n from 1 to 25
-            # - add n to each member of the child shift key, modulo 25
-            n = random.randint(1, 25)
-            child_shifts = [(x+n) % 25 for x in child_shifts]
+            # randomly choose a number n from 1 to blocksize
+            # - add n to each member of the child shift key, modulo blocksize
+            n = random.randint(1, blocksize)
+            child_shifts = [(x+n) % blocksize for x in child_shifts]
         # d. decipher the ciphertext with the child keys
-        plaintext = decipher(ciphertext, child_shifts, child_perms)
+        plaintext = decipher(ciphertext, child_shifts,
+                             child_perms, keylen, blocksize)
         # e. calculate the new fitness of the new plaintext
         new_fitness = fitness(plaintext)
         # f. set the margin to be
@@ -162,6 +168,7 @@ def hill_climb(ciphertext):
         # ((the new fitness exceeds the best fitness minus the margin) and
         # (we roll a 1 on a 20-sided die))
         if new_fitness > best_fitness or (new_fitness > best_fitness-margin and random.randint(1, 20) == 1):
+            print("success")
             # i. copy the child shift key into the parent shift key
             parent_shifts = child_shifts
             # ii. copy the child permutation into the parent permutation
@@ -172,7 +179,23 @@ def hill_climb(ciphertext):
             count = 0
             # h. increment the counter
         count += 1
-    return [alphabet[x] for x in parent_shifts]
+    return plaintext
+
+
+def cribbed_decrypt(ciphertext, crib, keylen=5, blocksize=25):
+    # https://theblackchamber552383191.wordpress.com/2020/11/17/cadenus-extending-the-crib/
+    cipher_grid = [ciphertext[i:i+keylen]
+                   for i in range(0, len(ciphertext), keylen)]
+    cipher_columns = [x for x in zip(*cipher_grid)]
+    if len(crib) > keylen:
+        for _ in range(len(crib)):
+            crib = rotate(crib, 1)
+            crib_grid = [crib[i:i+keylen] for i in range(0, len(crib), keylen)]
+            crib_columns = [x for x in zip(*crib_grid)]
+            for crib_column, cipher_column in zip(crib_columns, cipher_columns):
+                if "".join(crib_column) in "".join(cipher_column):
+                    print("hit!", crib_column, crib, "".join(
+                        cipher_column).find("".join(crib_column)))
 
 
 encrypted = cadenus_encrypt("""TO BE OR NOT TO BE THAT IS THE QUESTION WHETHER TIS NOBLER
@@ -185,7 +208,10 @@ encrypted = cadenus_encrypt("""TO BE OR NOT TO BE THAT IS THE QUESTION WHETHER T
     THE RUB FOR IN THAT SLEEP OF DEATH WHAT DREAMS MAY COME
     WUTEVUH""".replace(" ", "").replace("\n", ""), "oratio")
 
+cribbed_decrypt(encrypted.lower(), "perchancetodream")
+
 assert (cadenus_decrypt(encrypted, "oratio") == "TOBEORNOTTOBETHATISTHEQUESTIONWHETHERTISNOBLERINTHEMINDTOSUFFERTHESLINGSANDARROWSOFOUTRAGEOUSFORTUNEORTOTAKEARMSAGAINSTASEAOFTROUBLESANDBYOPPOSINGENDTHEMTODIETOSLEEPNOMOREANDBYASLEEPTOSAYWEENDTHEHEARTACHEANDTHETHOUSANDNATURALSHOCKSTHATFLESHISHEIRTOTISACONSUMMATIONDEVOUTLYTOBEWISHDTODIETOSLEEPTOSLEEPPERCHANCETODREAMAYTHERESTHERUBFORINTHATSLEEPOFDEATHWHATDREAMSMAYCOMEWUTEVUH")  # noqa: E501
 
-print(hill_climb(encrypted))
-# print(cadenus_decrypt(open("final.txt").read().replace(" ", ""), "lzyix"))
+# print(hill_climb(encrypted))
+
+# print(cadenus_decrypt(open("final.txt").read().replace(" ", ""), word))
